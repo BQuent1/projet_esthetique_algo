14beaf97f36633e85939486d6899f28b81df3a24
BQuent1 <quentin.baud9@gmail.com>
2025-01-16 17:19:26 +0100

    homapge

---
+++ script_main.js
@@ -0,0 +1,46 @@
+function startExperience() {
+    window.location.href = 'index.html'; // Remplacez par l'URL réelle de votre expérience
+}
+
+const body = document.querySelector('body');
+const btnStart = document.querySelector('.btn-start');
+
+let cursorIsVisible = true; // Suivi de l'état de visibilité du curseur
+const hiddenCursorDistance = 100; // Distance à partir de laquelle le curseur disparaît
+
+body.addEventListener('mousemove', (e) => {
+    // Calcul de la distance entre le curseur et le bouton
+    const distanceX = e.clientX - btnStart.getBoundingClientRect().left - btnStart.offsetWidth / 2;
+    const distanceY = e.clientY - btnStart.getBoundingClientRect().top - btnStart.offsetHeight / 2;
+    const distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+
+    if (distance < hiddenCursorDistance && cursorIsVisible) {
+        document.body.style.cursor = 'none'; // Cache le curseur
+        cursorIsVisible = false;
+        btnStart.classList.add('selected');
+
+    } else if (distance >= hiddenCursorDistance) {
+        document.body.style.cursor = 'none'; // Cache le curseur
+        cursorIsVisible = true;
+        btnStart.classList.remove('selected');
+
+        const trail = document.createElement('div');
+        trail.classList.add('trail');
+        trail.style.left = `${e.clientX}px`;
+        trail.style.top = `${e.clientY}px`;
+        document.body.appendChild(trail);
+
+        setTimeout(() => {
+            trail.remove();
+        }, 1000);
+
+    }
+});
+
+// Réactiver le curseur lorsque l'on clique sur le bouton
+btnStart.addEventListener('click', () => {
+    if (!cursorIsVisible) {
+        document.body.style.cursor = 'auto'; // Affiche à nouveau le curseur
+        cursorIsVisible = true;
+    }
+});
